1bb4782cd96eebd689af145b1f342eb7e9d20c7f
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-26 14:03:59 +0800

    update

---
+++ solutions/Hard/41. First Missing Positive/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        // write your code in C++14 (g++ 6.2.0)
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
+    }
+};
