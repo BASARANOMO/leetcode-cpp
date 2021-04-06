7ccfa90f8b07688420a0629a65d36d2b8b34dea0
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-04-06 21:29:51 +0800

    rename function

--- solutions/Hard/4. Median of Two Sorted Arrays/solution2.cpp
+++ solutions/Hard/4. Median of Two Sorted Arrays/solution2.cpp
@@ -1,27 +1,30 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    int binarySearchHelper(vector<int>& nums1, vector<int>& nums2, int pos) {
+    int findKthElem(vector<int>& nums1, vector<int>& nums2, int k) {
         int m = nums1.size(), n = nums2.size();
         int idx1 = 0, idx2 = 0;
         int new_idx1, new_idx2;
         while (true) {
-            if (idx1 == m) return nums2[idx2 + pos - 1];
-            if (idx2 == n) return nums1[idx1 + pos - 1];
-            if (pos == 1) return min(nums1[idx1], nums2[idx2]);
-            new_idx1 = min(m - 1, idx1 + pos / 2 - 1);
-            new_idx2 = min(n - 1, idx2 + pos / 2 - 1);
+            if (idx1 == m) return nums2[idx2 + k - 1];
+            if (idx2 == n) return nums1[idx1 + k - 1];
+            if (k == 1) return min(nums1[idx1], nums2[idx2]);
+            new_idx1 = min(m - 1, idx1 + k / 2 - 1);
+            new_idx2 = min(n - 1, idx2 + k / 2 - 1);
             if (nums1[new_idx1] <= nums2[new_idx2]) {
-                pos -= new_idx1 - idx1 + 1;
+                k -= new_idx1 - idx1 + 1;
                 idx1 = new_idx1 + 1;
             } else {
-                pos -= new_idx2 - idx2 + 1;
+                k -= new_idx2 - idx2 + 1;
                 idx2 = new_idx2 + 1;
             }
         }
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size();
-        if ((m + n) & 1) return binarySearchHelper(nums1, nums2, (m + n) / 2 + 1);
-        else return (binarySearchHelper(nums1, nums2, (m + n) / 2) + binarySearchHelper(nums1, nums2, (m + n) / 2 + 1)) / 2.0;
+        if ((m + n) & 1) return findKthElem(nums1, nums2, (m + n) / 2 + 1);
+        else return (findKthElem(nums1, nums2, (m + n) / 2) + findKthElem(nums1, nums2, (m + n) / 2 + 1)) / 2.0;
     }
 };
