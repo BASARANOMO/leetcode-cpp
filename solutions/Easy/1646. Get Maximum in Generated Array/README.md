6087a32c320dca184225a4d7910bdbe7d7494401
M. ZHANG <39518094+BASARANOMO@users.noreply.github.com>
2021-08-28 16:06:51 +0800

    update solution to question 1646

--- solutions/Easy/1646. Get Maximum in Generated Array/README.md
+++ solutions/Easy/1646. Get Maximum in Generated Array/README.md
@@ -2,3 +2,4 @@
 
 ## Solution
 
+Simulation
--- solutions/Easy/1646. Get Maximum in Generated Array/solution.cpp
+++ solutions/Easy/1646. Get Maximum in Generated Array/solution.cpp
@@ -6,13 +6,13 @@ public:
     int getMaximumGenerated(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-        vector<int> nums {0, 1};
+        vector<int> nums(n + 1);
+        nums[1] = 1;
         int ans = 1, curr;
         for (int i = 2; i < n + 1; i++) {
-            if ((i & 1) == 0) curr = nums[i / 2];
-            else curr = nums[i / 2] + nums[i / 2 + 1];
+            curr = nums[i / 2] + (i % 2) * nums[i / 2 + 1];
             ans = curr > ans ? curr : ans;
-            nums.push_back(curr);
+            nums[i] = curr;
         }
         return ans;
     }
